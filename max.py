5e0c8f6b4ea72da18a10fcd078aab2bb7d0fc8f8
reddymodi <114898950+reddymodi@users.noreply.github.com>
2023-04-06 15:10:25 +0530

    Create max.py

---
+++ max.py
@@ -0,0 +1,12 @@
+n=list(map(int,input("enter ur list: ").split()))
+k=sorted(n)
+e=[]
+o=[]
+for i in k:
+    if i%2==0:
+        e += [i]
+    else:
+        o += [i]
+print("your list is : ",n)
+print("even largest no:",e[len(e)-1])
+print("odd largest no: ",o[len(o)-1])
